1381881d67325bdf2ffc5d2e3a16a7297585dbf2
Raymond Willett <raymond.s.willett@accenture.com>
2019-07-22 14:32:23 -0400

    tests for post and getting corresponding direction

--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,20 @@ const http = require('http');
 const express = require('express');
 
 const app = express(http);
+direction = '';
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile("./views/index.html", {root: __dirname });
 })
 
 app.get('/command', (req, res) => {
-    res.sendStatus(200);
+    res.send(direction);
 })
 
 app.post('/command', (req, res) => {
+    direction = req.body.direction;
     res.sendStatus(200);
 })
 
--- Server/spec/server.spec.js
+++ Server/spec/server.spec.js
@@ -2,27 +2,58 @@ var request = require("request");
 
 var base_url = "http://localhost:3000/"
 
-describe("server", function () {
-  
-  describe("GET /command", function () {
-  
-    it("responds with 200 status", function (done) {
-  
-      request.get(`${base_url}command`, function (error, response, body) {
+describe("server", () => {
+
+  describe("GET /command", () => {
+
+    it("responds with 200 status", done => {
+
+      request.get(`${base_url}command`, (error, response, body) => {
         expect(response.statusCode).toBe(200);
         done();
       });
     });
   });
 
-  describe("POST /command", function () {
-  
-    it("responds with 200 status", function (done) {
-  
-      request.post(`${base_url}command`, function (error, response, body) {
+  describe("POST /command", () => {
+
+    it("responds with 200 status", done => {
+
+      request.post(`${base_url}command`, (error, response, body) => {
         expect(response.statusCode).toBe(200);
         done();
       });
+    });
+
+  });
+
+  describe("storing a turn command", () => {
+    it("stores a left turn command", done => {
+      request.post(`${base_url}command`,
+        {
+          json: {
+            direction: 'left'
+          }
+        }, (error, response, body) => {
+          request.get(`${base_url}command`, (error, response, body) => {
+            expect(body).toBe('left');
+            done()
+          });
+        })
+    });
+
+    it("stores a right turn command", done => {
+      request.post(`${base_url}command`,
+        {
+          json: {
+            direction: 'right'
+          }
+        }, (error, response, body) => {
+          request.get(`${base_url}command`, (error, response, body) => {
+            expect(body).toBe('right');
+            done()
+          });
+        })
     });
   });
 });
