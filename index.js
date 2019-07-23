82165de0c30aeb6bdedb4da6ead74638b02faac3
Raymond Willett <raymond.s.willett@accenture.com>
2019-07-23 10:17:47 -0400

    Add frontend.js script

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile("./views/index.html", {root: __dirname });
 })
 
+app.get('/scripts/frontend.js', (req, res) => {
+    res.sendFile("./views/scripts/frontend.js", {root: __dirname });
+})
+
 app.get('/command', (req, res) => {
     res.send(direction);
     direction = ''
