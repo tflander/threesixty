119a8d0c8cc49d13a28cade87cbd55911e2cfc46
Raymond Willett <raymond.s.willett@accenture.com>
2019-07-24 16:05:37 -0400

    Add buffer

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const express = require('express');
+import ActionBuffer from "./util/ActionBuffer";
 
 const PORT = process.env.PORT || 3000
+const MAX_BUFFER_SIZE = 3;
 
 const app = express(http);
-direction = '';
+const buffer = new ActionBuffer(MAX_BUFFER_SIZE);
 
 app.use(express.json())
 
@@ -22,11 +24,10 @@ app.get('/static/style.css', (req, res) => {
 
 app.get('/command', (req, res) => {
     res.send(direction);
-    direction = ''
 })
 
 app.post('/command', (req, res) => {
-    direction = req.body.direction;
+    buffer.push(req.body.direction);
     res.sendStatus(200);
 })
 
--- util/ActionBuffer.js
+++ util/ActionBuffer.js
@@ -2,7 +2,7 @@ const maxSizeSymbol = Symbol('Max Size');
 const queueSymbol = Symbol('queue')
 
 export default class ActionBuffer {
-    constructor(maxSize=3) {
+    constructor(maxSize = 3) {
         this[maxSizeSymbol] = maxSize;
         this[queueSymbol] = new Array();
     }
